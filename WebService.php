32761e5e648f8bd52c93269e4648020c24b0bcd7
jessseash <45877873+jessseash@users.noreply.github.com>
2018-12-14 10:55:40 -0500

    Update WebService.php

--- WebService.php
+++ WebService.php
@@ -4,7 +4,7 @@ header("Content-Type:application/json");
 //require "data.php";
 header("HTTP/1.1 ",200);
 
-$inputJSON = file_get_contents('php://input');
+$inputJSON = file_get_contents('php://input'); //captures the Json object
 $input = json_decode($inputJSON, TRUE); //convert JSON into array
 //extract hash values here
 $response['data']=$input["name"]." was here..."; ///concatinates a string verifying the service succesfully executed
